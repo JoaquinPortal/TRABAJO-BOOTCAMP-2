3d4c54a4e2507b475930333c632552b1d805c9ad
JoaquinPortal <99630851+JoaquinPortal@users.noreply.github.com>
2022-02-21 00:17:04 -0500

    Add files via upload

---
+++ TRABAJO-BOOTCAMP 2/script.js
@@ -0,0 +1,10 @@
+
+function contactus(){
+    ModalContactus = document.querySelector("#modal-contactanos");
+    ModalContactus.className = "show_modal";
+   }
+   
+function closecontactus(){
+    ModalContactus = document.querySelector("#modal-contactanos");
+    ModalContactus.className = "closemodal";
+   }
